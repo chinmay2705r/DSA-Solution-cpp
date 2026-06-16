c939adf57bc0f0ce90b0bfae3a38066996987c17
chinmay2705r <chinmaykumargupta.2707@gmail.com>
2026-06-16 21:51:45 +0530

    Longest Common Prefix

---
+++ arrays/15_Longest_Common_Prefix.cpp
@@ -0,0 +1,24 @@
+// Problem 15 : Longest Common Prefix
+// Leetcode : https://leetcode.com/problems/longest-common-prefix/
+// Difficulty : Easy
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        
+        string prefix = strs[0];
+        
+        for (size_t i = 1; i < strs.size(); ++i) {
+            while (strs[i].find(prefix) != 0) {
+                prefix = prefix.substr(0, prefix.length() - 1);
+                if (prefix.empty()) return "";
+            }
+        }
+        
+        return prefix;
+    }
+};
