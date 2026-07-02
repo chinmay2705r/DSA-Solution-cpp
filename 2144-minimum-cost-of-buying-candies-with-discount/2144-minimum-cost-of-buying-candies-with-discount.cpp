716a58c077f82b994f87b764d95bd40661ca8c1d
Chinmay Kumar Gupta <chinmaykumargupta.2707@gmail.com>
2026-07-02 12:19:42 +0530

    Time: 0 ms (100%), Space: 17 MB (7.92%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        
+        sort(cost.rbegin(), cost.rend());
+        
+        int totalCost = 0;
+        
+        for (int i = 0; i < cost.size(); i++) {
+         
+            if ((i + 1) % 3 == 0) {
+                continue; 
+            }
+            totalCost += cost[i];
+        }
+        
+        return totalCost;
+    }
+};
