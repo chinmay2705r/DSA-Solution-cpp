5f2dbe7237cb5126c4dc3799f05236edaa0617b3
Chinmay Kumar Gupta <chinmaykumargupta.2707@gmail.com>
2026-07-06 22:59:07 +0530

    Time: 4 ms (28.48%), Space: 15 MB (82.12%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        // Sort: ascending by start point; if tied, descending by end point
+        
         sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
             if (a[0] == b[0]) {
                 return a[1] > b[1];
@@ -13,11 +13,10 @@ public:
         int max_end = 0;
         
         for (const auto& interval : intervals) {
-            // If the current interval's end fits within the max_end seen so far, it's covered
+            
             if (interval[1] <= max_end) {
                 covered_count++;
             } else {
-                // Update the boundary for tracking subsequent intervals
                 max_end = interval[1];
             }
         }
