05472d9eea158de2328d070ff322ff525fbdf72d
Chinmay Kumar Gupta <chinmaykumargupta.2707@gmail.com>
2026-07-06 23:04:41 +0530

    Time: 0 ms (100%), Space: 14.1 MB (38.47%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,37 +1,39 @@
 class Solution {
-private:
-    void backtrack(int index, int target, vector<int>& candidates, 
-                   vector<int>& current, vector<vector<int>>& result) {
-     
-        if (target == 0) {
-            result.push_back(current);
-            return;
-        }
-
-        for (int i = index; i < candidates.size(); ++i) {
-            
-            if (candidates[i] > target) {
-                break;
-            }
-
-            if (i > index && candidates[i] == candidates[i - 1]) {
-                continue;
-            }
-
-            current.push_back(candidates[i]);      
-            backtrack(i + 1, target - candidates[i], candidates, current, result); 
-            current.pop_back(); 
-        }
-    }
-
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>> result;
         vector<int> current;
         
+        
         sort(candidates.begin(), candidates.end());
         
-        backtrack(0, target, candidates, current, result);
+        
+        auto backtrack = [&](auto& self, int index, int remaining_target) -> void {
+            
+            if (remaining_target == 0) {
+                result.push_back(current);
+                return;
+            }
+            
+            for (int i = index; i < candidates.size(); ++i) {
+               
+                if (candidates[i] > remaining_target) {
+                    break;
+                }
+             
+                if (i > index && candidates[i] == candidates[i - 1]) {
+                    continue;
+                }
+                
+                current.push_back(candidates[i]);               // Choose
+                self(self, i + 1, remaining_target - candidates[i]); 
+                current.pop_back();                             
+            }
+        };
+        
+       
+        backtrack(backtrack, 0, target);
+        
         return result;
     }
 };
