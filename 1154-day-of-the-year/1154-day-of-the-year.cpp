f902214096be18806ca8bfe82e00497b9eb33563
Chinmay Kumar Gupta <chinmaykumargupta.2707@gmail.com>
2026-07-31 23:09:32 +0530

    Time: 0 ms (100%), Space: 12 MB (78.42%) - LeetHub

---
+++ 1154-day-of-the-year/1154-day-of-the-year.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dayOfYear(string date) {
+        int year = stoi(date.substr(0, 4));
+        int month = stoi(date.substr(5, 2));
+        int day = stoi(date.substr(8, 2));
+
+        vector<int> daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        bool isLeap = (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+        if (isLeap) {
+            daysInMonth[1] = 29;
+        }
+
+        int dayCount = day;
+        for (int i = 0; i < month - 1; ++i) {
+            dayCount += daysInMonth[i];
+        }
+
+        return dayCount;
+    }
+};
