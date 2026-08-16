503cc9b776995baa282d06f1cf422c6973acf7fe
Chinmay Kumar Gupta <chinmaykumargupta.2707@gmail.com>
2026-08-16 09:28:19 +0530

    Time: 3 ms (75.38%), Space: 131.2 MB (69.29%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0,0,0};
+
+        for (int stone : stones){
+            cnt[stone % 3]++;
+        }
+        if(cnt[0] % 2 == 0){
+            return cnt[1] >= 1 && cnt [2] >= 1;
+        }
+        return(cnt[1] - cnt[2] >= 3) || (cnt[2] - cnt[1] >= 3);   
+    }
+};
